bbee322f93ed74b4c46201c2a82e7e6720f06dee
Lambert <lambertkuang@maidgency.com>
2016-02-13 00:12:42 -0800

    Complete prot problem

---
+++ python/rosalind_prot.py
@@ -0,0 +1,29 @@
+codon_protein = {
+    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC':'V',
+    'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+    'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+    'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+    'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+    'UAA': '', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+    'UAG': '', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+    'UGA': '', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+    'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'
+}
+
+with open(r'../data/rosalind_prot.txt','r') as my_file:
+    s = my_file.readline()
+    n = 3
+    s = [s[i:i+n] for i in range(0, len(s), n)]
+    s.remove('\n')
+    protein = ''
+    for codon in s:
+        protein += codon_protein[codon]
+    print protein
+
